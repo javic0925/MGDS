989f4700830dc8ca09cb9e7406a31981f5ef48f1
Javier Cruz <javic0925@gmail.com>
2022-02-08 12:55:15 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,71 @@
-// VERSION: 2
+var CACHE_NAME = 'my-site-cache-v10';
+var urlsToCache = [
+  '.audius.js',
+  '.likes.js',
+  '.script.js'
+];
 
-// Installing service worker
-const CACHE_NAME  = '4';
-
-/* Add relative URL of all the static content you want to store in
- * cache storage (this will help us use our app offline)*/
-let resourcesToCache = [ "./"];
-
-self.addEventListener("install", e=>{
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache =>{
-            return cache.addAll(resourcesToCache);
-        })
-    );
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 // Cache and return requests
-self.addEventListener("fetch", e=>{
-    e.respondWith(
-        caches.match(e.request).then(response=>{
-            return response || fetch(e.request);
-        })
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
     );
 });
 
 // Update a service worker
-const cacheWhitelist = ['Impac-Records-Radio'];
-self.addEventListener('activate', event => {
-    event.waitUntil(
-      caches.keys().then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  });
+self.addEventListener('activate', function(event) {
+
+  var cacheAllowlist = ['my-site-cache-v10'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
